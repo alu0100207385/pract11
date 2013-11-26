83ecffe8293dfed18370a999b4bcc95a1faac16a
Aaron Socas Gaspar <alu0100207385@ull.edu.es>
2013-11-26 21:25:43 +0000

    añado Rakefile + lib con las modificaciones y nuevas funciones

---
+++ lib/matriz.rb
@@ -0,0 +1,37 @@
+
+class Matriz
+  attr_reader :fil, :col
+  
+  def initialize (m,n) #Crea la matriz con las dimensiones especificadas por parametro
+    raise TypeError, "Error. Tipo de dimensiones incorrectas" if ((m < 0) or (n < 0))
+    @fil, @col = m, n
+  end
+  
+  def to_s
+  end
+  
+  def introducir_datos (o)
+  end
+  
+  def generar
+  end
+  
+  def +(other)
+  end
+  
+  def -(other)
+  end
+
+  def *(other)
+  end
+  
+  def t
+  end
+  
+  def det
+  end
+  
+  def ==(other)
+  end
+
+end
